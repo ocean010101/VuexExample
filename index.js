857382b1c5e70ecab542cfa49d8bd8e07a96b1ed
ocean010101 <yangyang01.xu@outlook.com>
2019-10-03 10:55:55 +0800

    Init：Add Counter
    Signed-off-by: ocean010101 <yangyang01.xu@outlook.com>

---
+++ index.js
@@ -0,0 +1,27 @@
+// make sure to call Vue.use(Vuex) if using a module system
+const store = new Vuex.Store({
+    state: {
+        count: 0
+    },
+    mutations: {
+        increment: state => state.count++,
+        decrement: state => state.count--
+    }
+})
+
+new Vue({
+    el: '#app',
+    computed: {
+        count() {
+            return store.state.count
+        }
+    },
+    methods: {
+        increment() {
+            store.commit('increment')
+        },
+        decrement() {
+            store.commit('decrement')
+        }
+    }
+})
